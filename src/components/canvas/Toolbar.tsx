ebddb368c95b3b8890622f0bc63a0894bf8c7bcd
dracu-lah <nevilnicks4321@gmail.com>
2025-03-25 17:42:05 +0530

    fixed some issues

--- src/components/canvas/Toolbar.tsx
+++ src/components/canvas/Toolbar.tsx
@@ -5,6 +5,7 @@ import { useCanvas } from "../../context/CanvasContext";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "../ui/tabs";
 import { TableIcon, Square, DoorOpen, Minus } from "lucide-react";
 import { ElementData } from "@/types";
+import TableIcons from "@/utils/assets";
 
 export const Toolbar: React.FC = () => {
   const { state, dispatch } = useCanvas();
@@ -16,24 +17,82 @@ export const Toolbar: React.FC = () => {
     let shape = "rounded-full";
 
     switch (tableType) {
-      case "round":
+      case "hexagon-6":
         color = "bg-amber-700";
         shape = "rounded-full";
+        elementWidth = 36;
+        elementHeight = 36;
         break;
-      case "square":
-        color = "bg-blue-600";
+      case "oval-6":
+        color = "bg-purple-600";
+        shape = "rounded-full";
+        elementWidth = 48;
+        elementHeight = 32;
+        break;
+      case "oval-8":
+        color = "bg-purple-600";
+        shape = "rounded-full";
+        elementWidth = 56;
+        elementHeight = 36;
+        break;
+      case "rectangle-2":
+        color = "bg-green-600";
         shape = "rounded-md";
+        elementWidth = 24;
+        elementHeight = 32;
         break;
-      case "rectangular":
+      case "rectangle-4":
+        color = "bg-green-600";
+        shape = "rounded-md";
+        elementWidth = 32;
+        elementHeight = 32;
+        break;
+      case "rectangle-6":
         color = "bg-green-600";
         shape = "rounded-md";
         elementWidth = 48;
         elementHeight = 32;
         break;
-      case "oval":
-        color = "bg-purple-600";
+      case "rectangle-8":
+        color = "bg-green-600";
+        shape = "rounded-md";
+        elementWidth = 64;
+        elementHeight = 32;
+        break;
+      case "round-3":
+        color = "bg-amber-700";
+        shape = "rounded-full";
+        elementWidth = 24;
+        elementHeight = 24;
+        break;
+      case "round-6":
+        color = "bg-amber-700";
+        shape = "rounded-full";
+        elementWidth = 36;
+        elementHeight = 36;
+        break;
+      case "round-8":
+        color = "bg-amber-700";
+        shape = "rounded-full";
+        elementWidth = 40;
+        elementHeight = 40;
+        break;
+      case "round-9":
+        color = "bg-amber-700";
+        shape = "rounded-full";
+        elementWidth = 44;
+        elementHeight = 44;
+        break;
+      case "round-10":
+        color = "bg-amber-700";
         shape = "rounded-full";
         elementWidth = 48;
+        elementHeight = 48;
+        break;
+      case "square-4":
+        color = "bg-blue-600";
+        shape = "rounded-md";
+        elementWidth = 32;
         elementHeight = 32;
         break;
     }
@@ -115,37 +174,109 @@ export const Toolbar: React.FC = () => {
           </TabsTrigger>
         </TabsList>
         <TabsContent value="tables" className="flex space-x-2">
-          <Button
-            variant="outline"
-            onClick={() => addTable("round")}
-            className="flex items-center"
-          >
-            <div className="h-4 w-4 bg-amber-700 rounded-full mr-2"></div>
-            Round Table
+          <Button variant="outline" onClick={() => addTable("hexagon-6")}>
+            <img
+              src={TableIcons.TableHexagon6Icon}
+              alt="Hexagon 6"
+              className="h-4 w-4 mr-2"
+            />
+            Hexagon 6
           </Button>
-          <Button
-            variant="outline"
-            onClick={() => addTable("square")}
-            className="flex items-center"
-          >
-            <div className="h-4 w-4 bg-blue-600 rounded-md mr-2"></div>
-            Square Table
+          <Button variant="outline" onClick={() => addTable("oval-6")}>
+            <img
+              src={TableIcons.TableOval6Icon}
+              alt="Oval 6"
+              className="h-4 w-4 mr-2"
+            />
+            Oval 6
           </Button>
-          <Button
-            variant="outline"
-            onClick={() => addTable("rectangular")}
-            className="flex items-center"
-          >
-            <div className="h-4 w-6 bg-green-600 rounded-md mr-2"></div>
-            Rectangular Table
+          <Button variant="outline" onClick={() => addTable("oval-8")}>
+            <img
+              src={TableIcons.TableOval8Icon}
+              alt="Oval 8"
+              className="h-4 w-4 mr-2"
+            />
+            Oval 8
           </Button>
-          <Button
-            variant="outline"
-            onClick={() => addTable("oval")}
-            className="flex items-center"
-          >
-            <div className="h-4 w-6 bg-purple-600 rounded-full mr-2"></div>
-            Oval Table
+          <Button variant="outline" onClick={() => addTable("rectangle-2")}>
+            <img
+              src={TableIcons.TableRectangle2Icon}
+              alt="Rectangle 2"
+              className="h-4 w-4 mr-2"
+            />
+            Rectangle 2
+          </Button>
+          <Button variant="outline" onClick={() => addTable("rectangle-4")}>
+            <img
+              src={TableIcons.TableRectangle4Icon}
+              alt="Rectangle 4"
+              className="h-4 w-4 mr-2"
+            />
+            Rectangle 4
+          </Button>
+          <Button variant="outline" onClick={() => addTable("rectangle-6")}>
+            <img
+              src={TableIcons.TableRectangle6Icon}
+              alt="Rectangle 6"
+              className="h-4 w-4 mr-2"
+            />
+            Rectangle 6
+          </Button>
+          <Button variant="outline" onClick={() => addTable("rectangle-8")}>
+            <img
+              src={TableIcons.TableRectangle8Icon}
+              alt="Rectangle 8"
+              className="h-4 w-4 mr-2"
+            />
+            Rectangle 8
+          </Button>
+          <Button variant="outline" onClick={() => addTable("round-3")}>
+            <img
+              src={TableIcons.TableRound3Icon}
+              alt="Round 3"
+              className="h-4 w-4 mr-2"
+            />
+            Round 3
+          </Button>
+          <Button variant="outline" onClick={() => addTable("round-6")}>
+            <img
+              src={TableIcons.TableRound6Icon}
+              alt="Round 6"
+              className="h-4 w-4 mr-2"
+            />
+            Round 6
+          </Button>
+          <Button variant="outline" onClick={() => addTable("round-8")}>
+            <img
+              src={TableIcons.TableRound8Icon}
+              alt="Round 8"
+              className="h-4 w-4 mr-2"
+            />
+            Round 8
+          </Button>
+          <Button variant="outline" onClick={() => addTable("round-9")}>
+            <img
+              src={TableIcons.TableRound9Icon}
+              alt="Round 9"
+              className="h-4 w-4 mr-2"
+            />
+            Round 9
+          </Button>
+          <Button variant="outline" onClick={() => addTable("round-10")}>
+            <img
+              src={TableIcons.TableRound10Icon}
+              alt="Round 10"
+              className="h-4 w-4 mr-2"
+            />
+            Round 10
+          </Button>
+          <Button variant="outline" onClick={() => addTable("square-4")}>
+            <img
+              src={TableIcons.TableSquare4Icon}
+              alt="Square 4"
+              className="h-4 w-4 mr-2"
+            />
+            Square 4
           </Button>
         </TabsContent>
         <TabsContent value="objects" className="flex space-x-2">
