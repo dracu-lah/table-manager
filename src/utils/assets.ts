b2b86ae1eb45f29aa620351f15fb649d75faf8e5
dracu-lah <nevilnicks4321@gmail.com>
2025-03-25 17:41:59 +0530

    added table assets

---
+++ src/utils/assets.ts
@@ -0,0 +1,31 @@
+import TableHexagon6Icon from "@/assets/table-icons/hexagon-6.svg";
+import TableOval6Icon from "@/assets/table-icons/oval-6.svg";
+import TableOval8Icon from "@/assets/table-icons/oval-8.svg";
+import TableRectangle2Icon from "@/assets/table-icons/rectangle-2.svg";
+import TableRectangle4Icon from "@/assets/table-icons/rectangle-4.svg";
+import TableRectangle6Icon from "@/assets/table-icons/rectangle-6.svg";
+import TableRectangle8Icon from "@/assets/table-icons/rectangle-8.svg";
+import TableRound3Icon from "@/assets/table-icons/round-3.svg";
+import TableRound6Icon from "@/assets/table-icons/round-6.svg";
+import TableRound8Icon from "@/assets/table-icons/round-8.svg";
+import TableRound9Icon from "@/assets/table-icons/round-9.svg";
+import TableRound10Icon from "@/assets/table-icons/round-10.svg";
+import TableSquare4Icon from "@/assets/table-icons/square-4.svg";
+
+const TableIcons = {
+  TableHexagon6Icon,
+  TableOval6Icon,
+  TableOval8Icon,
+  TableRectangle2Icon,
+  TableRectangle4Icon,
+  TableRectangle6Icon,
+  TableRectangle8Icon,
+  TableRound3Icon,
+  TableRound6Icon,
+  TableRound8Icon,
+  TableRound9Icon,
+  TableRound10Icon,
+  TableSquare4Icon,
+};
+
+export default TableIcons;
