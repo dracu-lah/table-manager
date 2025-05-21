e5c30e7eb588d24afaeff1713ad62963837ec9dd
dracu-lah <nevilnicks4321@gmail.com>
2025-05-21 17:02:08 +0530

    updated private routes

--- src/router/routes/privateRoutes.tsx
+++ src/router/routes/privateRoutes.tsx
@@ -2,12 +2,50 @@ import { lazy } from "react";
 import routePath from "../routePath";
 import { ProtectedRoute } from "../ProtectedRoute";
 import { Navigate } from "react-router";
-
+const CustomerViewPage = lazy(
+  () =>
+    import("@/pages/restaurants/[restaurantId]/areas/[areaId]/customer-view"),
+);
+const RestaurantsPage = lazy(() => import("@/pages/restaurants"));
+const RestaurantDetailsPage = lazy(
+  () => import("@/pages/restaurants/[restaurantId]"),
+);
+const AreaCanvasViewPage = lazy(
+  () => import("@/pages/restaurants/[restaurantId]/areas/[areaId]/canvas"),
+);
 // Lazy load private route components
 export const privateRoutes = [
   {
     path: "/",
     element: <ProtectedRoute />,
-    children: [{ index: true, element: "hello" }],
+    children: [
+      { index: true, element: "hello" },
+      // Define nested routes within RootLayout
+      {
+        index: true, // This route will match at the root path ("/")
+        element: <Navigate to="/restaurants" replace />, // Redirect to /restaurants
+      },
+      {
+        path: "/restaurants/:restaurantId/areas/:areaId/customer-view",
+        element: <CustomerViewPage />,
+      },
+      {
+        path: "/restaurants",
+        element: <RestaurantsPage />,
+      },
+      {
+        path: "/restaurants/:id",
+        element: <RestaurantDetailsPage />,
+      },
+      {
+        path: "/restaurants/:restaurantId/areas/:areaId/canvas",
+        element: <AreaCanvasViewPage />,
+      },
+      // Add a catch-all route for 404 (optional)
+      {
+        path: "*",
+        element: <div>404 Not Found</div>,
+      },
+    ],
   },
 ];
