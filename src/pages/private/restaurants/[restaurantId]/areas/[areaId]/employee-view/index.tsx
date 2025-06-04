e4f05ccc69192d59270afab290e3f87110a48d1d
dracu-lah <nevilnicks4321@gmail.com>
2025-06-04 15:30:54 +0530

    moved to another file

--- src/pages/private/restaurants/[restaurantId]/areas/[areaId]/employee-view/index.tsx
+++ src/pages/private/restaurants/[restaurantId]/areas/[areaId]/employee-view/index.tsx
@@ -12,7 +12,8 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-import { RoomLayouts, TableIcons as Tables } from "@/utils/assets";
+import { RoomLayouts } from "@/utils/assets";
+import getTableIcon from "./getTableIcon";
 
 // Table status configurations
 const TABLE_STATUSES = {
@@ -56,39 +57,6 @@ const TABLE_STATUSES = {
 
 // Table Icon Component with Switch Case
 const TableIcon = ({ tableType, status, width, height }) => {
-  const getTableIcon = (type) => {
-    switch (type) {
-      case "hexagon-6":
-        return Tables.TableHexagon6Icon;
-      case "oval-6":
-        return Tables.TableOval6Icon;
-      case "oval-8":
-        return Tables.TableOval8Icon;
-      case "rectangle-2":
-        return Tables.TableRectangle2Icon;
-      case "rectangle-4":
-        return Tables.TableRectangle4Icon;
-      case "rectangle-6":
-        return Tables.TableRectangle6Icon;
-      case "rectangle-8":
-        return Tables.TableRectangle8Icon;
-      case "round-3":
-        return Tables.TableRound3Icon;
-      case "round-6":
-        return Tables.TableRound6Icon;
-      case "round-8":
-        return Tables.TableRound8Icon;
-      case "round-9":
-        return Tables.TableRound9Icon;
-      case "round-10":
-        return Tables.TableRound10Icon;
-      case "square-4":
-        return Tables.TableSquare4Icon;
-      default:
-        return null;
-    }
-  };
-
   const IconComponent = getTableIcon(tableType);
   const textColor = TABLE_STATUSES[status]?.textColor || "text-gray-600";
 
