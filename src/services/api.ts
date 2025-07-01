429550d5fd2cb23f8573004ad07c1a5475282287
dracu <dracu@thonkpad.thonkpad>
2025-07-01 08:39:57 +0530

    fix:added endpoints for outlets

--- src/services/api.ts
+++ src/services/api.ts
@@ -219,7 +219,7 @@ export const GetRolesAPI = async (params: any) => {
 
 export const GetOutletsAPI = async (params: any) => {
   try {
-    const { data } = await api.get(`${endPoint.getRoles}`, {
+    const { data } = await api.get(`${endPoint.getOutlets}`, {
       params: params,
     });
     return data;
